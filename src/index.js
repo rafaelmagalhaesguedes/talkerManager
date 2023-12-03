4abaddbd720ee7a5ab20d6b7fefec559185b6628
Rafael <rafaelmagalhaes1@live.com>
2023-12-03 20:44:37 -0300

    feat: requirement 04

--- src/index.js
+++ src/index.js
@@ -47,6 +47,19 @@ app.get('/talker/:id', async (request, response) => {
 // POST /login endpoint
 app.post('/login', (request, response) => {
   const { email, password } = request.body;
+  if (!email) {
+    return response.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  const emailRegex = /\S+@\S+\.\S+/;
+  if (!emailRegex.test(email)) {
+    return response.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) {
+    return response.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return response.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
   const token = crypto.randomBytes(8).toString('hex');
   return response.status(HTTP_OK_STATUS).json({ token });
 });
