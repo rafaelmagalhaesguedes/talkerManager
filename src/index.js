5681051254658a4d10467862ca723dc254dc398f
Rafael <rafaelmagalhaes1@live.com>
2023-12-04 14:14:12 -0300

    refact: src/index

--- src/index.js
+++ src/index.js
@@ -1,67 +1,15 @@
 const express = require('express');
-const fs = require('fs').promises;
-const path = require('path');
-const crypto = require('crypto');
+
+const routers = require('./routers');
 
 const app = express();
-app.use(express.json());
 
-const HTTP_OK_STATUS = 200;
+app.use(routers);
+
 const PORT = process.env.PORT || '3001';
-const readTalkers = path.resolve(__dirname, './talker.json');
 
-// não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
-});
-
-// GET /talker endpoint
-app.get('/talker', async (_request, response) => {
-  try {
-    const data = await fs.readFile(readTalkers, 'utf-8');
-    const talkers = JSON.parse(data);
-    return response.status(HTTP_OK_STATUS).json(talkers);
-  } catch (error) {
-    console.error(`Error: ${error}`);
-    return response.status(HTTP_OK_STATUS).json([]);
-  }
-});
-
-// GET /talker/:id endpoint
-app.get('/talker/:id', async (request, response) => {
-  const { id } = request.params;
-  try {
-    const data = await fs.readFile(readTalkers, 'utf-8');
-    const talkers = JSON.parse(data);
-    const talker = talkers.find((element) => element.id === Number(id));
-    if (!talker) {
-      return response.status(404).json({ message: 'Pessoa palestrante não encontrada' });
-    }
-    return response.status(HTTP_OK_STATUS).json(talker);
-  } catch (error) {
-    console.error(`Error: ${error}`);
-    return response.status(HTTP_OK_STATUS).json([]);
-  }
-});
-
-// POST /login endpoint
-app.post('/login', (request, response) => {
-  const { email, password } = request.body;
-  if (!email) {
-    return response.status(400).json({ message: 'O campo "email" é obrigatório' });
-  }
-  const emailRegex = /\S+@\S+\.\S+/;
-  if (!emailRegex.test(email)) {
-    return response.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
-  }
-  if (!password) {
-    return response.status(400).json({ message: 'O campo "password" é obrigatório' });
-  }
-  if (password.length < 6) {
-    return response.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
-  }
-  const token = crypto.randomBytes(8).toString('hex');
-  return response.status(HTTP_OK_STATUS).json({ token });
+  response.status(200).send();
 });
 
 app.listen(PORT, () => {
