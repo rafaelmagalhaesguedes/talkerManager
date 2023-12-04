d2fa9e2f9938cdf429df83ba36e20461a9cc7c7c
Rafael <rafaelmagalhaes1@live.com>
2023-12-04 18:38:44 -0300

    refact: create middleware for validation Login

--- src/routers/login.js
+++ src/routers/login.js
@@ -6,22 +6,29 @@ const router = express.Router();
 
 router.use(express.json());
 
-// POST /login endpoint
-router.post('/login', (request, response) => {
-  const { email, password } = request.body;
-  if (!email) {
-    return response.status(400).json({ message: 'O campo "email" é obrigatório' });
-  }
+// Middleware for validation
+function validateLoginRequest(req, res, next) {
+  const { email, password } = req.body;
   const emailRegex = /\S+@\S+\.\S+/;
+
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
   if (!emailRegex.test(email)) {
-    return response.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
   if (!password) {
-    return response.status(400).json({ message: 'O campo "password" é obrigatório' });
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
   }
   if (password.length < 6) {
-    return response.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
+
+  next();
+}
+
+// POST /login endpoint
+router.post('/login', validateLoginRequest, (request, response) => {
   const token = crypto.randomBytes(8).toString('hex');
   return response.status(200).json({ token });
 });
