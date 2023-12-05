d374c0973ffc586fc0981b22eed7de0b3b1bd5a4
Rafael <rafaelmagalhaes1@live.com>
2023-12-05 20:22:52 -0300

    feat: create dir /utils

--- src/utils/talker/filters.js
+++ src/utils/talker/filters.js
@@ -1,3 +1,7 @@
+const fs = require('fs').promises;
+const path = require('path');
+const readTalkerFile = require('./reader');
+
 module.exports = {
   filterBySearchTerm(talkers, q) {
     if (!q || q.trim() === '') return talkers;
@@ -25,5 +29,23 @@ module.exports = {
     }
 
     return filteredTalkers;
+  },
+
+  validateRateTalker(rate, res) {
+    if (!rate || !Number.isInteger(rate) || rate < 1 || rate > 5) {
+      return res.status(400)
+        .json({ message: 'O campo "rate" deve ser um número inteiro entre 1 e 5' });
+    }
+  },
+  
+  async findTalkerIndex(id) {
+    const talkers = await readTalkerFile();
+    return talkers.findIndex((talker) => talker.id === Number(id));
+  },
+  
+  async updateTalkerRate(talkerIndex, rate) {
+    const talkers = await readTalkerFile();
+    talkers[talkerIndex].talk.rate = rate;
+    await fs.writeFile(path.join(__dirname, '..', 'talker.json'), JSON.stringify(talkers));
   },
 };
