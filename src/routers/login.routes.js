b4ec6da10ad7386750d8baba49c3d45d8ec245af
Rafael <rafaelmagalhaes1@live.com>
2023-12-04 22:38:37 -0300

    refact: login routes

--- src/routers/login.routes.js
+++ src/routers/login.routes.js
@@ -1,36 +1,14 @@
 const express = require('express');
-
 const crypto = require('crypto');
+const { validateEmail, validatePassword } = require('../middlewares/login/loginValidation');
 
 const router = express.Router();
 
 router.use(express.json());
 
-// Middleware for validation
-function validateLoginRequest(req, res, next) {
-  const { email, password } = req.body;
-  const emailRegex = /\S+@\S+\.\S+/;
-
-  if (!email) {
-    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
-  }
-  if (!emailRegex.test(email)) {
-    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
-  }
-  if (!password) {
-    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
-  }
-  if (password.length < 6) {
-    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
-  }
-
-  next();
-}
-
-// POST /login endpoint
-router.post('/login', validateLoginRequest, (request, response) => {
+router.post('/login', validateEmail, validatePassword, (_req, resp) => {
   const token = crypto.randomBytes(8).toString('hex');
-  return response.status(200).json({ token });
+  return resp.status(200).json({ token });
 });
 
 module.exports = router;
