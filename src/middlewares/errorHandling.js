60d45fe26254025ef44c3166b10ace157660e6e4
Rafael <rafaelmagalhaes1@live.com>
2023-12-04 22:46:19 -0300

    fix: parameters errorHandler

--- src/middlewares/errorHandling.js
+++ src/middlewares/errorHandling.js
@@ -1,4 +1,4 @@
-module.exports = function errorHandler(err, req, res, _next) {
+module.exports = function errorHandler(err, _req, res, _next) {
   console.error(err);
   res.status(500).json({ message: 'Internal Server Error' });
 };
