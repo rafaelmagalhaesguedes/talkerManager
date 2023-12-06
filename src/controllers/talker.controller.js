8a855cf0160c1b5dfe56264e5cc75ea3c24aafb7
Rafael <rafaelmagalhaes1@live.com>
2023-12-06 10:16:19 -0300

    fix: rename filter.js to talkerFilters.js

--- src/controllers/talker.controller.js
+++ src/controllers/talker.controller.js
@@ -1,7 +1,7 @@
 const fs = require('fs').promises;
 const path = require('path');
 const validation = require('../middlewares/talker.validation');
-const filter = require('../utils/filters');
+const filter = require('../utils/talkerFilters');
 
 const readTalkerFile = path.resolve(__dirname, '../talker.json');
 
